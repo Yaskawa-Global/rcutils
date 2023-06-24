527fd16858a89659b0a7eb1d6ff8cd83ddbc2178
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2023-06-24 19:12:56 +0200

    Avoid clash with M+ macros
    Note: max(..) does not appear to be used.

--- src/sha256.c
+++ src/sha256.c
@@ -17,12 +17,12 @@
 
 #include "rcutils/sha256.h"
 
-static inline size_t min(size_t a, size_t b)
+static inline size_t __min(size_t a, size_t b)
 {
   return a < b ? a : b;
 }
 
-static inline size_t max(size_t a, size_t b)
+static inline size_t __max(size_t a, size_t b)
 {
   return a > b ? a : b;
 }
@@ -140,7 +140,7 @@ void rcutils_sha256_update(rcutils_sha256_ctx_t * ctx, const uint8_t * data, siz
   while (i < len) {
     data_remaining = len - i;
     block_remaining = 64 - ctx->datalen;
-    copy_len = min(min(block_remaining, data_remaining), 64);
+    copy_len = __min(__min(block_remaining, data_remaining), 64);
 
     memcpy(ctx->data + ctx->datalen, data + i, copy_len);
     ctx->datalen += copy_len;
